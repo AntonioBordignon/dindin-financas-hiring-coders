57d9d43861f4d70d4b0787e4fb469fca7ff2aeed
Antonio Bordignon <antoniovictorbordignon@outlook.com>
2022-07-05 16:34:14 -0300

    fixes of style

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import {useParams} from "react-router-dom"
 import Header from "../../components/Header"
+import Footer from "../../components/Footer"
 
 export default function Contato() {
     const params = useParams();
     return(
         <div>
-            <Header />
-            <h1>Página de contato</h1>
-            <h2>Olá {params.nome}</h2>
         </div>
     );
 }
